c212db96ee9d4e4d7d3fb00130bdd93fd4da5874
unknown <biswajit2013laskar@gmail.com>
2023-09-01 22:08:08 +0530

    ok

--- app/Http/Controllers/Api/getInformationController.php
+++ app/Http/Controllers/Api/getInformationController.php
@@ -3,11 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Constituency;
+use App\Models\District;
+use App\Models\GpDetail;
 use Illuminate\Http\Request;
 
 class getInformationController extends Controller
 {
    public function info(){
-
+      $district = District::get();
+      $constituent = Constituency::get();
+      $gp = GpDetail::get();
+      $information = [
+        'district'    => $district,
+        'constituent' => $constituent,
+        'information' => $gp,
+      ];
+      return response()->json([
+        'status' => true,
+        'message' => 'User Created Successfully',
+        'token' => $user->createToken("API TOKEN")->plainTextToken
+    ], 200);
    }
 }
