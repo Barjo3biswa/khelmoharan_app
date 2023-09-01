2a90a615d065752ca669bbc95858b17ee350a173
unknown <biswajit2013laskar@gmail.com>
2023-09-01 22:28:25 +0530

    ok

--- app/Http/Controllers/Api/getInformationController.php
+++ app/Http/Controllers/Api/getInformationController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Models\Constituency;
 use App\Models\District;
 use App\Models\GpDetail;
+use App\Models\PlayerDetail;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class getInformationController extends Controller
@@ -22,7 +24,39 @@ class getInformationController extends Controller
       return response()->json([
         'status' => true,
         'message' => 'User Created Successfully',
-        'token' => $user->createToken("API TOKEN")->plainTextToken
+        'data'    => $information,
     ], 200);
    }
+
+   public function sendUserInfo(){
+
+    $authUserId = auth()->user()->id;
+
+    try{
+
+        $userInfo = User::query()
+            ->where('id', $authUserId)
+            ->first();
+        $is_record = PlayerDetail::where('user_id',$authUserId)->count();
+        if($is_record>0){
+            return response()->json([
+                'status' => true,
+                'message' => 'Record Found',
+                'data' => $userInfo
+            ], 200);
+        }else{
+            return response()->json([
+                'status' => false,
+                'message' => 'Record Not Found'
+            ], 404);
+        }
+
+    } catch (\Throwable $th) {
+        return response()->json([
+            'status' => false,
+            'message' => $th->getMessage()
+        ], 500);
+    }
+
+}
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\getInformationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,4 +24,7 @@ Route::post('/auth/register', [AuthController::class, 'createUser']);
 Route::post('/auth/login', [AuthController::class, 'loginUser']);
 
 Route::get('/get-dropdown-data', [getInformationController::class, 'info'])->middleware('auth:sanctum');
+
+Route::get('/get-user-info', [getInformationController::class, 'sendUserInfo'])->middleware('auth:sanctum');
+
 
